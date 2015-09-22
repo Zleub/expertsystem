3cd11528b8e3a10afee5a8be505f753d3b3076bb
Arthur Maurer <arthimaurer@gmail.com>
2015-09-22 03:18:05 +0300

    Solver rework (now using a tree)

--- solver.lua
+++ solver.lua
@@ -1,106 +1,145 @@
+-- (P & Q) -> R <-> P -> (Q->R)
+--
+-- local tree = {
+-- 	operator = "<=>",
+-- 	left = {
+-- 		operator = "=>",
+-- 		left = {
+-- 			operator = "+",
+-- 			left = "P",
+-- 			right = "Q"
+-- 		},
+-- 		right = "R"
+-- 	},
+-- 	right = {
+-- 		operator = "=>",
+-- 		left = "P",
+-- 		right = {
+-- 			operator = "=>",
+-- 			left = "Q",
+-- 			right = "R"
+-- 		}
+-- 	}
+-- }
+-- local variables = { "P", "Q", "R" }
 
-local conditions = {};
 
-conditions[1] = {
-	content = "=>",
-	left = "A",
+-- ((A v B) & (A & C) v D ) & (C)
+
+local tree = {
+	operator = "+",
+	left = {
+		operator = "|",
+		left = {
+			operator = "+",
+			left = {
+				operator = "|",
+				left = "A",
+				right = "B"
+			},
+			right = {
+				operator = "+",
+				left = "A",
+				right = "C"
+			}
+		},
+		right = "D"
+	},
 	right = "C"
 }
-conditions[2] = {
-	content = "=>",
-	left = {
-		content = "+",
-		left = "A",
-		right = "B"
-	},
-	right = "D"
-}
-
 local variables = { "A", "B", "C", "D" }
 
-local constrains = {
-	A = true,
-	B = false,
-}
-
+local constrains = {}
 local values = {}
+local solutions = {}
 
-function checkValues()
-	for key, value in pairs(constrains) do
-		if (values[key] ~= value) then
-			return false
-		end
-	end
-
-	return true
+for i, value in ipairs(variables) do
+	values[value] = false
 end
 
-function debug()
+function debug(values)
+	print("\t--------------")
 	for i, value in ipairs(variables) do
-		print(value, values[value])
+		print("\t  ".. value, values[value])
+	end
+	print("\t--------------")
+end
+
+function addSolution()
+	local index = #solutions + 1
+
+	solutions[index] = {}
+
+	for i, value in ipairs(variables) do
+		solutions[index][value] = values[value]
 	end
 end
 
-function testStep(index)
-	if index == nil then
-		return false
-	elseif type(index) == "string" then
-		return values[index]
-	end
-
-	local condition = conditions[index]
-	local content = condition.content;
-	local left = testStep(condition.left)
-	local right = testStep(condition.right)
-
-	if content == "+" then
+function testValues(operator, left, right)
+	if operator == "+" then
 		return left and right
-	elseif content == "|" then
+	elseif operator == "|" then
 		return left or right
-	elseif content == "!" then
+	elseif operator == "!" then
 		return not left
-	elseif content == "^" then
+	elseif operator == "^" then
 		return not left == right
-	elseif content == "=>" then
+	elseif operator == "=>" then
 		return not left or right
-	elseif content == "<=>" then
+	elseif operator == "<=>" then
 		return left == right
-	elseif content >= "A" and content <= "Z" then
-		return values[content]
-	else
+	end
+
+	return false
+end
+
+function test(tree)
+	local left = tree.left
+	local right = tree.right
+
+	if type(left) == "table" then
+		left = test(left)
+	elseif type(left) == "string" then
+		left = values[left]
+	end
+
+	if type(right) == "table" then
+		right = test(right)
+	elseif type(right) == "string" then
+		right = values[right]
+	end
+
+	return testValues(tree.operator, left, right)
+end
+
+function solve(index)
+	if index == nil then
+		solve(#variables)
 		return false
 	end
-end
 
-function testProgram()
-	for index, condition in ipairs(conditions) do
-		if testStep(index) == false then
-			return false
-		end
-	end
-	return true
-end
-
-function test(index)
 	if index == 0 then
-		return testProgram()
+		if test(tree) then
+			addSolution();
+		end
+		return false
 	end
 
 	local char = variables[index]
 
 	if constrains[char] ~= nil then
 		values[char] = constrains[char]
-		if test(index - 1) == true then
+		if solve(index - 1) == true then
 			return true
 		end
 	else
 		values[char] = false
-		if test(index - 1) == true then
+		if solve(index - 1) == true then
 			return true
 		end
 
 		values[char] = true
-		if test(index - 1) == true then
+		if solve(index - 1) == true then
 			return true
 		end
 	end
@@ -109,11 +148,11 @@ function test(index)
 end
 
 print("\nTesting...")
+solve()
+print("\nResult: ".. #solutions .." solution(s)\n")
 
-local result = test(#variables)
-
-print("\nResult:", result, "\n")
-
-if result then
-	debug()
+if #solutions > 0 then
+	for i in pairs(solutions) do
+		debug(solutions[i])
+	end
 end
