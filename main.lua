fde2a99105fa1cad2811bfa8a7990eed7428cc76
Zleub <debray.arnaud@gmail.com>
2016-05-08 18:15:59 +0200

    to do

--- main.lua
+++ main.lua
@@ -13,14 +13,42 @@
 --      .+ydddddddddhs/.
 --          .-::::-`
 
-gmr = require 'gmr'
+solve = require 'solver'
 inspect = require 'inspect'
 
-function debug(...) print(inspect({...})) end
+function debug(...) print(inspect(...)) end
+
+local operators = {
+	"!",
+	"%+",
+	"|",
+	"%^",
+	"=>",
+	"<=>"
+}
+
+function getTree(line)
+	print(line)
+	if line:match('%(.+%)') then getTree(line:match('%((.+)%)')) end
+
+	local tree = {}
+	for i,v in ipairs(operators) do
+		local l, o, r = line:match('(.+)('..v..')(.+)')
+		if o then table.insert(tree, { operator = o, left = getTree(l), right = getTree(r) }) end
+	end
+end
 
 if not arg[1] then
 	print('Provide file plz')
 else
-	tree = gmr(arg[1])
-	dofile('solver.lua')
+
+	for l in io.lines(arg[1]) do
+		debug( getTree(l) )
+	end
+
+	solve({
+		operator = "=>",
+		left = 'A',
+		right = 'B'
+	}, {'A', 'B'}, {['A'] = true})
 end
--- solver.lua
+++ solver.lua
@@ -26,7 +26,8 @@
 
 -- ((A v B) & (A & C) v D ) & (C)
 
-local tree = {
+local tree =
+{
 	operator = "=>",
 	left = 'A',
 	right = 'B'
@@ -56,110 +57,115 @@ local tree = {
 
 local variables = { "A", "B" }
 
-local constrains = {}
-local values = {}
-local solutions = {}
 
-for i, value in ipairs(variables) do
-	values[value] = false
-end
-
-function debug(values)
-	print("\t--------------")
-	for i, value in ipairs(variables) do
-		print("\t  ".. value, values[value])
-	end
-	print("\t--------------")
-end
-
-function addSolution()
-	local index = #solutions + 1
-
-	solutions[index] = {}
+function solve(tree, variables, constrains)
+	local constrains = constrains or {}
+	local values = {}
+	local solutions = {}
 
 	for i, value in ipairs(variables) do
-		solutions[index][value] = values[value]
-	end
-end
-
-function testValues(operator, left, right)
-	if operator == "+" then
-		return left and right
-	elseif operator == "|" then
-		return left or right
-	elseif operator == "!" then
-		return not left
-	elseif operator == "^" then
-		return not left == right
-	elseif operator == "=>" then
-		return not left or right
-	elseif operator == "<=>" then
-		return left == right
+		values[value] = false
 	end
 
-	return false
-end
-
-function test(tree)
-	local left = tree.left
-	local right = tree.right
-
-	if type(left) == "table" then
-		left = test(left)
-	elseif type(left) == "string" then
-		left = values[left]
+	function debug(values)
+		print("\t--------------")
+		for i, value in ipairs(variables) do
+			print("\t  ".. value, values[value])
+		end
+		print("\t--------------")
 	end
 
-	if type(right) == "table" then
-		right = test(right)
-	elseif type(right) == "string" then
-		right = values[right]
+	function addSolution()
+		local index = #solutions + 1
+
+		solutions[index] = {}
+
+		for i, value in ipairs(variables) do
+			solutions[index][value] = values[value]
+		end
 	end
 
-	return testValues(tree.operator, left, right)
-end
+	function testValues(operator, left, right)
+		if operator == "+" then
+			return left and right
+		elseif operator == "|" then
+			return left or right
+		elseif operator == "!" then
+			return not left
+		elseif operator == "^" then
+			return not left == right
+		elseif operator == "=>" then
+			return not left or right
+		elseif operator == "<=>" then
+			return left == right
+		end
 
-function solve(index)
-	if index == nil then
-		solve(#variables)
 		return false
 	end
 
-	if index == 0 then
-		if test(tree) then
-			addSolution();
+	function test(tree)
+		local left = tree.left
+		local right = tree.right
+
+		if type(left) == "table" then
+			left = test(left)
+		elseif type(left) == "string" then
+			left = values[left]
 		end
+
+		if type(right) == "table" then
+			right = test(right)
+		elseif type(right) == "string" then
+			right = values[right]
+		end
+
+		return testValues(tree.operator, left, right)
+	end
+
+	function solve(index)
+		if index == nil then
+			solve(#variables)
+			return false
+		end
+
+		if index == 0 then
+			if test(tree) then
+				addSolution();
+			end
+			return false
+		end
+
+		local char = variables[index]
+
+		if constrains[char] ~= nil then
+			values[char] = constrains[char]
+			if solve(index - 1) == true then
+				return true
+			end
+		else
+			values[char] = false
+			if solve(index - 1) == true then
+				return true
+			end
+
+			values[char] = true
+			if solve(index - 1) == true then
+				return true
+			end
+		end
+
 		return false
 	end
 
-	local char = variables[index]
+	print("\nTesting...")
+	solve()
+	print("\nResult: ".. #solutions .." solution(s)\n")
 
-	if constrains[char] ~= nil then
-		values[char] = constrains[char]
-		if solve(index - 1) == true then
-			return true
-		end
-	else
-		values[char] = false
-		if solve(index - 1) == true then
-			return true
-		end
-
-		values[char] = true
-		if solve(index - 1) == true then
-			return true
+	if #solutions > 0 then
+		for i in pairs(solutions) do
+			debug(solutions[i])
 		end
 	end
-
-	return false
 end
 
-print("\nTesting...")
-solve()
-print("\nResult: ".. #solutions .." solution(s)\n")
-
-if #solutions > 0 then
-	for i in pairs(solutions) do
-		debug(solutions[i])
-	end
-end
+return solve
